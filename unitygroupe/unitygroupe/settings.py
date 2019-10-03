152f81f03e0d8e55b508969cda399550a74441d3
youzerssif <39623776+youzerssif@users.noreply.github.com>
2019-10-03 16:03:40 +0000

    modele
    contact, config, client

--- unitygroupe/unitygroupe/settings.py
+++ unitygroupe/unitygroupe/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield',   
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_admin_generator',
 ]
 
 MIDDLEWARE = [
